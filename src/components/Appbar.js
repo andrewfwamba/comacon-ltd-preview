6cdc8587e01883cc09079443c41c8b8307051cf7
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-08-15 14:47:37 +0300

    mobile menu

--- src/components/Appbar.js
+++ src/components/Appbar.js
@@ -1,37 +1,110 @@
-import React from "react";
+import React, { useState } from "react";
 import { BsChevronDown } from "react-icons/bs";
 
 function Appbar() {
+  const [open, setOpen] = useState(true);
   return (
     <div>
-      <div className="h-[4rem] bg-white mt-2 justify-center items-center mb-8 flex w-full shadow-lg">
-        <div className="flex space-x-8 ml-10">
-          <ul className="flex space-x-4 tracking-tight text-gray-900 font-[300]">
-            <li className="flex space-x-3 justify-center items-center cursor-pointer hover:border-b-2 pb-4 border-blue-700">
-              <p>About</p> <BsChevronDown />
-            </li>
-            <li className="flex space-x-3 justify-center items-center cursor-pointer hover:border-b-2 pb-4 border-blue-700">
-              <p>Markets</p> <BsChevronDown />
-            </li>
-            <li className="flex space-x-3 justify-center items-center cursor-pointer hover:border-b-2 pb-4 border-blue-700">
-              <p>Services</p> <BsChevronDown />
-            </li>
-            <li className="flex space-x-3 justify-center items-center cursor-pointer hover:border-b-2 pb-4 border-blue-700">
-              <p>Products</p> <BsChevronDown />
-            </li>
-            <li className="flex space-x-3 justify-center items-center cursor-pointer hover:border-b-2 pb-4 border-blue-700">
-              <p>Lifecycle</p> <BsChevronDown />
-            </li>
-            <li className="flex space-x-3 justify-center items-center cursor-pointer hover:border-b-2 pb-4 border-blue-700">
-              <p>Parts Store</p> <BsChevronDown />
-            </li>
-            <li className="flex space-x-3 justify-center items-center cursor-pointer hover:border-b-2 pb-4 border-blue-700">
-              <p>ServTech</p>
-            </li>
-            <li className="flex space-x-3 justify-center items-center cursor-pointer hover:border-b-2 pb-4 border-blue-700">
-              <p>Method Flow</p>
-            </li>
-          </ul>
+      <div className="bg-white mt-2 px-4 md:px-1 mb-8 w-full shadow-md">
+        {/* Desktop menu */}
+        <div className="space-x-8 ml-10 hidden md:block">
+          <div className="flex justify-between">
+            <div className="text-gray-500">
+              <a href="/">Comacon &trade;</a>
+            </div>
+            <ul className="flex space-x-4 tracking-tight text-gray-900 font-[300]">
+              <li className="flex space-x-3 justify-center items-center cursor-pointer hover:border-b-2 pb-4 border-blue-700">
+                <p>About</p> <BsChevronDown />
+              </li>
+              <li className="flex space-x-3 justify-center items-center cursor-pointer hover:border-b-2 pb-4 border-blue-700">
+                <p>Markets</p> <BsChevronDown />
+              </li>
+              <li className="flex space-x-3 justify-center items-center cursor-pointer hover:border-b-2 pb-4 border-blue-700">
+                <p>Services</p> <BsChevronDown />
+              </li>
+              <li className="flex space-x-3 justify-center items-center cursor-pointer hover:border-b-2 pb-4 border-blue-700">
+                <p>Products</p> <BsChevronDown />
+              </li>
+              <li className="flex space-x-3 justify-center items-center cursor-pointer hover:border-b-2 pb-4 border-blue-700">
+                <p>Lifecycle</p> <BsChevronDown />
+              </li>
+              <li className="flex space-x-3 justify-center items-center cursor-pointer hover:border-b-2 pb-4 border-blue-700">
+                <p>Parts Store</p> <BsChevronDown />
+              </li>
+              <li className="flex space-x-3 justify-center items-center cursor-pointer hover:border-b-2 pb-4 border-blue-700">
+                <p>ServTech</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        {/* Mobile menu */}
+        <div className="block md:hidden py-2">
+          <div className="flex w-full justify-between ">
+            <div>
+              <p>Comacon Ltd</p>
+            </div>
+            <div onClick={() => setOpen(!open)}>
+              {open ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                  />
+                </svg>
+              )}
+            </div>
+          </div>
+          {open && (
+            <div>
+              <ul className="flex flex-col mt-5 bg-white w-full text-gray-900 font-[300]">
+                <li className="flex space-x-3 justify-left items-center cursor-pointer hover:border-b-2 pb-4 border-blue-700">
+                  <p>About</p> <BsChevronDown />
+                </li>
+                <li className="flex space-x-3 justify-between items-center cursor-pointer active:border-b-2 pb-4 border-blue-700">
+                  <p>Markets</p> <BsChevronDown />
+                </li>
+                <li className="flex space-x-3 justify-between items-center cursor-pointer active:border-b-2 pb-4 border-blue-700">
+                  <p>Services</p> <BsChevronDown />
+                </li>
+                <li className="flex space-x-3 justify-between items-center cursor-pointer active:border-b-2 pb-4 border-blue-700">
+                  <p>Products</p> <BsChevronDown />
+                </li>
+                <li className="flex space-x-3 justify-between items-center cursor-pointer active:border-b-2 pb-4 border-blue-700">
+                  <p>Lifecycle</p> <BsChevronDown />
+                </li>
+                <li className="flex space-x-3 justify-between items-center cursor-pointer active:border-b-2 pb-4 border-blue-700">
+                  <p>Parts Store</p> <BsChevronDown />
+                </li>
+                <li className="flex space-x-3 justify-between items-center cursor-pointer active:border-b-2 pb-4 border-blue-700">
+                  <p>ServTech</p>
+                </li>
+              </ul>
+            </div>
+          )}
         </div>
       </div>
     </div>
