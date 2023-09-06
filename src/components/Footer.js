80e332450d877bb326dd4ec9d01490d242ed31d5
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-09-06 18:09:53 +0300

    new footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { Button, Checkbox, Form, Input, Modal } from "antd";
-import { AiFillLinkedin } from "react-icons/ai";
+import { AiFillLinkedin, AiOutlinePhone, AiOutlineMail } from "react-icons/ai";
 import { BiLogoFacebook } from "react-icons/bi";
 import { BsTwitter, BsInstagram } from "react-icons/bs";
+import { CiLocationOn } from "react-icons/ci";
 
 function Footer() {
   const [loading, setLoading] = useState(false);
@@ -30,113 +31,110 @@ function Footer() {
         <div className="">
           <p
             onClick={showModal}
-            className="w-fit p-2 mx-auto border border-gray-400 rounded-md shadow-lg text-gray-700 hover:text-gray-300 hover:bg-gray-800 cursor-pointer"
+            className="w-fit p-2 mx-auto border border-green-400 rounded-md shadow-lg text-green-700 hover:text-gray-300 hover:bg-green-800 cursor-pointer"
           >
             Signup to our NewsLetter
           </p>
         </div>
       </div>
-      <div className="bg-[rgb(40,43,47)]/60 p-2 md:p-10">
-        <div className="grid gap-4 grid-cols-2 md:grid-cols-4 w-full px-8 md:w-2/3 mx-auto text-gray-200">
-          <div>
-            <p className="text-[18px] underline">Link Group 1</p>
+      <div className="bg-[url('https://unsplash.com/photos/41X6FwTwPh4/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8MTB8fGluZHVzdHJ5fGVufDB8fHx8MTY5Mzg0NTc4N3ww&force=true&w=1920')]">
+        <div className="bg-green-900/90 p-2 md:p-10">
+          <div className="grid gap-4 md:grid-cols-4 w-full px-2 md:px-8 mx-auto text-gray-200">
             <div>
-              <ul>
-                <li className="cursor-pointer my-2 text-[14px]">
-                  Sample Link 1
-                </li>
-                <li className="cursor-pointer my-2 text-[14px]">
-                  Sample Link 2
-                </li>
-                <li className="cursor-pointer my-2 text-[14px]">
-                  Sample Link 3
-                </li>
-                <li className="cursor-pointer my-2 text-[14px]">
-                  {" "}
-                  Sample Link 4
-                </li>
-                <li className="cursor-pointer my-2 text-[14px]">
-                  Sample Link 5
-                </li>
-              </ul>
+              <p className="text-[18px] text-green-200 border-b border-r pb-2 border-green-600">
+                About Comacon
+              </p>
+              <div>
+                <ul>
+                  <li className="cursor-pointer line-clamp-[8] my-2 text-[10px]">
+                    With roots in East Africa dating back to January 2007,
+                    Comacon Limited is a leading professional Civil, Mechanical,
+                    and Electrical contractor. Comacon Limited is now an
+                    affiliate of Building, Structural & Mass Concrete Casting,
+                    which is ultimately a part of the leading building
+                    technology and related services in the globe. Projects of
+                    any scale or complexity can be taken up by Comacon Limited
+                    anywhere in East Africa thanks to its resources and
+                    capacity. The best value for money solutions are ensured by
+                    a highly qualified and experienced team of civil,
+                    mechanical, and electrical engineers working with a team of
+                    operations professionals that are equally qualified and
+                    motivated.
+                  </li>
+                </ul>
+              </div>
             </div>
-          </div>
 
-          <div>
-            <p className="text-[18px] underline">Link Group 2</p>
             <div>
-              <ul>
-                <li className="cursor-pointer my-2 text-[14px]">
-                  Sample Link 1
-                </li>
-                <li className="cursor-pointer my-2 text-[14px]">
-                  SampleLink 2
-                </li>
-                <li className="cursor-pointer my-2 text-[14px]">
-                  Sample Link 3
-                </li>
-                <li className="cursor-pointer my-2 text-[14px]">
-                  Sample Link 4
-                </li>
-                <li className="cursor-pointer my-2 text-[14px]">
-                  Sample Link 5
-                </li>
-              </ul>
+              <p className="text-[18px] border-b border-r pb-2 border-green-600">
+                Also of Interest
+              </p>
+              <div>
+                <ul className="list-disc px-10">
+                  <li className="cursor-pointer my-2 text-[14px]">Civil</li>
+                  <li className="cursor-pointer my-2 text-[14px]">
+                    Mechanical
+                  </li>{" "}
+                  <li className="cursor-pointer my-2 text-[14px]">
+                    Electrical
+                  </li>{" "}
+                  <li className="cursor-pointer my-2 text-[14px]">Supplies</li>
+                </ul>
+              </div>
             </div>
-          </div>
 
-          <div>
-            <p className="text-[18px] underline">Link Group 3</p>
-            <div>
-              <ul>
-                <li className="cursor-pointer my-2 text-[14px]">
-                  Sample Link 1
-                </li>
-                <li className="cursor-pointer my-2 text-[14px]">
-                  Sample Link 2
-                </li>
-                <li className="cursor-pointer my-2 text-[14px]">
-                  Sample Link 3
-                </li>
-                <li className="cursor-pointer my-2 text-[14px]">
-                  Sample Link 4
-                </li>
-                <li className="cursor-pointer my-2 text-[14px]">
-                  Sample Link 5
-                </li>
-              </ul>
+            <div className="">
+              <p className="text-[18px] border-b border-r pb-2 border-green-600">
+                Open Hours
+              </p>
+              <div>
+                <ul className="text-[12px] py-3">
+                  <li className="py-2">Mon - Sat 8.00 to 18.00</li>
+                  <li>Sunday Closed</li>
+                </ul>
+              </div>
             </div>
-          </div>
 
-          <div>
-            <p className="text-[18px] underline">Link Group 4</p>
             <div>
-              <ul>
-                <li className="cursor-pointer my-2 text-[14px]">
-                  Sample Link 1
-                </li>
-                <li className="cursor-pointer my-2 text-[14px]">
-                  Sample Link 2
-                </li>
-                <li className="cursor-pointer my-2 text-[14px]">
-                  Sample Link 3
-                </li>
-                <li className="cursor-pointer my-2 text-[14px]">
-                  Sample Link 4
-                </li>
-                <li className="cursor-pointer my-2 text-[14px]">
-                  Sample Link 5
-                </li>
-              </ul>
+              <p className="text-[18px] border-b border-r pb-2 border-green-600">
+                Get in Touch
+              </p>
+              <div>
+                <ul>
+                  <li className="cursor-pointer flex space-x-2 my-2 text-[14px]">
+                    <CiLocationOn className="text-3xl md:text-2xl font-bold" />
+                    <div>
+                      <p className="text-[12px]">
+                        <span className="text-gray-300">Headquaters:</span>
+                        Lunga lunga Square, Lunga Lunga Road, Nairobi.
+                      </p>
+                    </div>
+                  </li>
+                  <li className="cursor-pointer flex space-x-2 my-2 text-[14px]">
+                    <AiOutlinePhone className="text-2xl font-bold" />
+                    <div>
+                      <p className="text-[12px]">+254- 722 675 477</p>
+                      <p className="text-[12px]">+254- 780 675 477</p>
+                    </div>
+                  </li>
+                  <li className="cursor-pointer flex space-x-2 my-2 text-[14px]">
+                    <AiOutlineMail className="text-xl font-bold" />
+                    <div>
+                      <p className="text-[14px]">info@comaconltd.com</p>
+                    </div>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
       </div>
+
       <div className="flex md:flex-row flex-col justify-between mt-6 md:mt-4 w-full px-2 mx-auto pb-2 text-[18px] font-[500]">
         <p className="text-[12px] text-gray-500">
           &copy; {new Date().getFullYear()} The Comacon Group Inc | Terms and
-          Conditions | Privacy Policy
-        </p>{" "}
+          Conditions
+        </p>
         <div className="flex justify-evenly text-gray-600 md:space-x-4 mt-6 md:mt-0">
           <a href="/" className="flex space-x-1 justify-center items-center">
             <AiFillLinkedin className="text-gray-700 text-[15px] " />{" "}
