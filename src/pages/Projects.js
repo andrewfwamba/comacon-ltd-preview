cf87edbd6fa4b1af1926b7db56176cbf18806860
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-09-06 18:10:50 +0300

    initial projects page

---
+++ src/pages/Projects.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Projects() {
+  return (
+    <div>
+      <div className="bg-[url('https://unsplash.com/photos/2EasWxTpxag/download?ixid=M3wxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjkzODk5ODQ1fA&force=true&w=640')]">
+        <div className="h-[7rem] flex items-center justify-center bg-green-950/50 px-[5rem]">
+          <p className="text-green-500 text-[2rem] animate__animated animate__fadeInRight md:text-[3rem] font-bold">
+            Our Projects
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Projects;
